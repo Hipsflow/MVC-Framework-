d074db9b6ec15e7a52f367cda7fa6ceb06fb172f
CauPis <caua@sitecbr.com.br>
2023-01-03 16:44:59 -0300

    Commit to debub database in other enviroment

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,11 @@
 <?php
+    //DB params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'postgres');
+    define('DB_PASS', 'cetis');
+    define('DB_NAME', 'testeMVC');
+    define('DB_PORT', '5432');
+
     // App root
      define('APPROOT', dirname(dirname(__FILE__)));
 
---
+++ app/libraries/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database {
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+    private $port = DB_PORT;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct() {
+        $dsn = 'pgsql:host=' . $this->host . ';dbname=' . $this->dbname;     
+        try{
+            $pdo = new PDO($dsn, $this->user, $this->password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        }catch(PDOException $e){
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+}
